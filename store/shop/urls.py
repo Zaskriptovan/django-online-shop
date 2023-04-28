cfe6a33338c4706c6be32439341cca66fc5b53c3
Zaskriptovan <zaskrip@example.com>
2023-04-28 18:32:06 +0300

    test class View

--- store/shop/urls.py
+++ store/shop/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('categories/<slug:category>/', views.categories, name='categories'),
+    path('', views.ShopHome.as_view(), name='home'),
+    # path('categories/<slug:category>/', views.categories, name='categories'),
+    path('categories/<slug:category>/', views.Categories.as_view(), name='categories'),
 ]
--- store/shop/views.py
+++ store/shop/views.py
@@ -1,13 +1,44 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 
-def index(request):
-    context = {
-        'title': 'Main Page',
-    }
-    return render(request, 'shop/index.html', context=context)
+# from .models import Product
 
 
-def categories(request, category):
-    return HttpResponse(f'<h2>{category}</h2>')
+class ShopHome(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+            'title': 'Main Page',
+        }
+        return render(request, 'shop/index.html', context=context)
+
+
+# def index(request):
+#     context = {
+#         'title': 'Main Page',
+#     }
+#     return render(request, 'shop/index.html', context=context)
+
+
+# class ShopHome(ListView):
+#     model = Product
+#     template_name = 'shop/index.html'
+#
+#     # extra_context = {'title': {'one': 2}, }
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['title'] = 'Main Page'
+#
+#         return context
+
+
+# def categories(request, category):
+#     return HttpResponse(f'<h2>{category}</h2>')
+
+
+class Categories(View):
+    def get(self, request, *args, **kwargs):
+        # return HttpResponse(f'<h2>{args}, {kwargs}</h2>')
+        return render(request, 'shop/categories.html', context=kwargs)
