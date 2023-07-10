3d85c7f4cce1d94c7be37a745498fae4e1ec8b20
Zaskriptovan <zaskrip@example.com>
2023-07-10 19:39:10 +0300

    refact models

--- store/cart/models.py
+++ store/cart/models.py
@@ -5,10 +5,28 @@ from shop.models import Product
 
 
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Пользователь')
-    total_price = models.PositiveIntegerField(default=0, verbose_name='Общая стоимость')
-    time_created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
-    time_updated = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
+    )
+
+    total_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+        verbose_name='Общая стоимость'
+    )
+
+    time_created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Время создания'
+    )
+
+    time_updated = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Время изменения'
+    )
 
     class Meta:
         ordering = ('time_created',)
@@ -20,14 +38,41 @@ class Cart(models.Model):
 
 
 class CartProduct(models.Model):
-    product = models.ForeignKey(Product, related_name='cart_products',
-                                on_delete=models.CASCADE, verbose_name='Товар')
-    quantity = models.PositiveIntegerField(default=1, verbose_name='Количество')
-    several_price = models.PositiveIntegerField(default=0, verbose_name='Цена за количество')
-    time_created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
-    time_updated = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
-    cart = models.ForeignKey('Cart', related_name='cart_products',
-                             on_delete=models.CASCADE, verbose_name='Корзина')
+    product = models.ForeignKey(
+        Product,
+        related_name='cart_products',
+        on_delete=models.CASCADE,
+        verbose_name='Товар'
+    )
+
+    quantity = models.PositiveIntegerField(
+        default=1,
+        verbose_name='Количество'
+    )
+
+    several_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0,
+        verbose_name='Цена за количество'
+    )
+
+    time_created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Время создания'
+    )
+
+    time_updated = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Время изменения'
+    )
+
+    cart = models.ForeignKey(
+        'Cart',
+        related_name='cart_products',
+        on_delete=models.CASCADE,
+        verbose_name='Корзина'
+    )
 
     class Meta:
         ordering = ('time_created',)
--- store/shop/models.py
+++ store/shop/models.py
@@ -2,17 +2,57 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=200, db_index=True, verbose_name='Название')
-    slug = models.SlugField(max_length=200, db_index=True, unique=True, verbose_name='URL')
-    image = models.ImageField(upload_to='products_images/%Y/%m/%d', blank=True, verbose_name='Изображение')
-    description = models.TextField(blank=True, verbose_name='Описание')
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
-    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
-    time_created = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
-    time_updated = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    title = models.CharField(
+        max_length=200,
+        db_index=True,
+        verbose_name='Название'
+    )
 
-    category = models.ForeignKey('Category', related_name='products', on_delete=models.PROTECT,
-                                 verbose_name="Категория")
+    slug = models.SlugField(
+        max_length=200,
+        db_index=True,
+        unique=True,
+        verbose_name='URL'
+    )
+
+    image = models.ImageField(
+        upload_to='products_images/%Y/%m/%d',
+        blank=True,
+        verbose_name='Изображение'
+    )
+
+    description = models.TextField(
+        blank=True,
+        verbose_name='Описание'
+    )
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name='Цена'
+    )
+
+    quantity = models.PositiveIntegerField(
+        default=0,
+        verbose_name='Количество'
+    )
+
+    time_created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Время создания'
+    )
+
+    time_updated = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Время изменения'
+    )
+
+    category = models.ForeignKey(
+        'Category',
+        related_name='products',
+        on_delete=models.PROTECT,
+        verbose_name="Категория"
+    )
 
     class Meta:
         ordering = ('title',)
@@ -24,8 +64,18 @@ class Product(models.Model):
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=200, db_index=True, verbose_name='Название')
-    slug = models.SlugField(max_length=200, db_index=True, unique=True, verbose_name='URL')
+    title = models.CharField(
+        max_length=200,
+        db_index=True,
+        verbose_name='Название'
+    )
+
+    slug = models.SlugField(
+        max_length=200,
+        db_index=True,
+        unique=True,
+        verbose_name='URL'
+    )
 
     class Meta:
         ordering = ('title',)
